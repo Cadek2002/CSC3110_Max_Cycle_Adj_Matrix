d24fa2b009858960b4b412e17e5b1a32c375dd9d
Cadek2002 <77899221+Cadek2002@users.noreply.github.com>
2023-11-12 17:22:34 -0500

    Allowed Vertices Map Functionality

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,8 @@ import java.util.stream.IntStream;
 
 public class Main {
 
-    public static ArrayList<ArrayList<Integer>> readFile(String inputFile) {
+    //File Input
+    public static ArrayList<ArrayList<Integer>> readFile(String inputFile, ArrayList<Integer> verticesMap) {
         try {
             Scanner matrixScanner = new Scanner(new File(inputFile));
             matrixScanner.useDelimiter(System.getProperty("line.separator"));
@@ -21,7 +22,7 @@ public class Main {
                 matrix.add(new ArrayList<>());
                 rowScanner = new Scanner(matrixScanner.next());
                 while (rowScanner.hasNextInt()) {
-                    matrix.get(matrix.size()-1).add(rowScanner.nextInt());
+                    matrix.get(matrix.size()-1).add(verticesMap == null ? rowScanner.nextInt() : verticesMap.indexOf(rowScanner.nextInt()));
                 }
             }
             matrixScanner.close();
@@ -31,12 +32,28 @@ public class Main {
             return null;
         }
     }
-    public static void exportFile(ArrayList<ArrayList<Integer>> matrix, String outputName) {
+    public static ArrayList<Integer> readVertices(String inputFile) {
+        try {
+            Scanner matrixScanner = new Scanner(new File(inputFile));
+            ArrayList<Integer> vertices= new ArrayList<>();
+            //Import Cycle Set into 2d Vector
+            while (matrixScanner.hasNext()) {
+                vertices.add(matrixScanner.nextInt());
+            }
+            matrixScanner.close();
+            return vertices;
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+    //File Output
+    public static void exportFile(ArrayList<ArrayList<Integer>> matrix, String outputName, ArrayList<Integer> verticesMap) {
         File output = new File(outputName);
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(output));
             for (ArrayList<Integer> row : matrix) {
-                for (Integer i : row) writer.write(String.format("%c ", i+48));
+                for (Integer i : row) writer.write(String.format("%c ", verticesMap == null ? i+48 : verticesMap.get(i)));
                 writer.newLine();
             }
             writer.close();
@@ -44,6 +61,7 @@ public class Main {
             System.out.println(e.getMessage());
         }
     }
+
     public static ArrayList<ArrayList<Integer>> createMatrix(int size, int min, int max, double lambda, String outputName) {
         //initiate matrix
         ArrayList<ArrayList<Integer>> output = new ArrayList<>(size);
@@ -75,9 +93,10 @@ public class Main {
             y = (element / size) + ((((element % size) + offset) >= size) ? 1 : 0);
             output.get(x).set(y, (int)(Math.random() * (max-min) + min));
         }
-        exportFile(output, outputName);
+        exportFile(output, outputName, null);
         return output;
     }
+    //Find Cycles Issues
     public static void findCycles(ArrayList<ArrayList<Integer>> adjMatrix, LinkedHashSet<Integer> path, int last, int first, ArrayList<LinkedHashSet<Integer>> set) {
         if (last == first && path.size() > 1 && !set.contains(path)) {
             set.add(path);
@@ -103,30 +122,37 @@ public class Main {
     public static void main(String[] args) {
         String matrixFileName = "inputMatrix.txt";
         String setFileName = "inputCycles.txt";
-
+        String verticesFileName = "vertices.txt";
         String outputName = "outputMatrix.txt";
 
-        ArrayList<ArrayList<Integer>> cycles = readFile(setFileName);
-        ArrayList<ArrayList<Integer>> adjMatrix = readFile(matrixFileName);
 
-        //GENERATES NEW MATRIX/CYCLES, if you want to keep a particular matrix comment theese out
+        ArrayList<Integer> vertices = readVertices(verticesFileName);
+        ArrayList<ArrayList<Integer>> cycles = readFile(setFileName, vertices);
+        ArrayList<ArrayList<Integer>> adjMatrix = readFile(matrixFileName, null);
+
+        //GENERATES NEW MATRIX/CYCLES, if you want to keep a particular matrix comment these out
         //Create Matrix (Size, min_val, max_val, sparsity, output file name)
         adjMatrix = createMatrix(9, 1, 9, .5, "matrix.txt");
         //Create Cycle Set
         cycles = findCycles(adjMatrix);
-        exportFile(cycles, "outCycles.txt");
+        exportFile(cycles, "outCycles.txt", null);
 
         if (cycles != null && adjMatrix != null) {
             Instant start = Instant.now();
             //CODE ALGORITHM HERE variables: cycles, adjMatrix both are ArrayList<ArrayList<Integer>>
+
+            for (ArrayList<Integer> cycle:cycles) {
+                for (int i : cycle) System.out.printf("%d ", i);
+                System.out.println();
+            }
 
             //END ALGO CODE
             Instant end = Instant.now();
 
             System.out.printf("Algo Run Duration: %s Nanoseconds", Duration.between(start, end).getNano());
 
-            //Export set into file
-            exportFile(adjMatrix, outputName);
+            //Export updated matrix into file
+            exportFile(adjMatrix, outputName, null);
         }
     }
 }
